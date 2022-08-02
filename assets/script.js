dfa3aef290d7492bea90b919707c3cce41adba53
Gaara <chrisimata21@gmail.com>
2022-08-01 20:16:32 -0700

    implemented local storage and add/remove styles based on time

---
+++ assets/script.js
@@ -0,0 +1,50 @@
+// displays current time
+function showTime() {
+    currentDay.innerText = moment().format('LLLL');
+}
+setInterval(showTime, 1000);
+
+// saves to local storage
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        let text = $(this).siblings(".description").val();
+        let time = $(this).parent().attr("id");
+
+        localStorage.setItem(time, text);
+    })
+
+
+    function timeKeep() {
+        let currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            let blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (blockTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            } else if (blockTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            } else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+timeKeep();
+})
